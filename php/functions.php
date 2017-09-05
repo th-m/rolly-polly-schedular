b285d2a58c222a863459e616d43b5dbcc1099047
thom <thom@MacBook-Air.local>
2017-09-05 11:35:48 -0600

    selected values started for dropdowns

--- php/functions.php
+++ php/functions.php
@@ -5,14 +5,25 @@
     return "eh?";
   }
   
-  function selectBoxOptionHelper($column){
+  function selectBoxOptionHelper($column, $row_id = Null, $parent_table){
     $associate_column = str_replace("_id","s",$column);
     $qry = "SELECT id, title FROM $associate_column";
     $assoc_table_info = sql_query($qry);
+    
+    //NOTE: get selected values
+    $selected_ids = "hello";
+    if($row_id != Null){
+      $qry = "SELECT room_id FROM $parent_table WHERE id = $row_id";
+      $selected_ids = sql_query($qry);
+    }
+    
     foreach ($assoc_table_info as $info) {
       $id = $info['id'];
       $title = $info['title'];
-      echo "<option id='$id' value='$id'> $title </option>";
+      // $selected = (($row_id != Null && $row_id == $id) ? 'selected': '');
+      // echo "<option id='$id' value='$id'> $title $selected_ids[0] </option>";
+      echo print_r($selected_ids);
     }
+    
   }
  ?>
--- php/views/components/form.php
+++ php/views/components/form.php
@@ -1,9 +1,24 @@
 <?php 
   include("../../functions.php"); 
-  $qry = "SELECT * FROM {$_POST['table']}";
+  if(isset($_POST['rowId'])){
+    $qry = "SELECT * FROM {$_POST['table']} WHERE id = {$_POST['rowId']}";  
+  }else{
+    $qry = "SELECT * FROM {$_POST['table']}";
+  }
+  $table_info = sql_query($qry);
+
+?>  
+<pre>
+<?php print_r($_POST); ?>
+</pre>
+<!-- <pre>
+<?php print_r($table_info); ?>
+</pre> -->
+<?php
   $table_info = sql_query($qry);
   $dontShowColumns = ['id', 'company_id'];
   $columns = array_diff(array_keys($table_info[0]), $dontShowColumns);
+
 ?>
 <!-- <pre>
 <?php print_r($columns); ?>
@@ -12,12 +27,12 @@
 <form class="" action="" >
   <?php foreach ($columns as $column) { 
     if(strpos($column, '_id')){  ?>
-    <select id="<?=$column?>">
-      <?php selectBoxOptionHelper($column); ?>
-    </select>   
+    <!-- <select id="<?=$column?>"> -->
+      <?php selectBoxOptionHelper($column, $_POST['rowId'], $_POST['table']); ?>
+    <!-- </select>    -->
     <br> 
   <?php } else { ?>
-    <input type="text" name="<?=$column?>" value="" placeholder="<?=$column?>">
+    <input type="text" name="<?=$column?>" value="<?=$table_info[0][$column]?>" placeholder="<?=$column?>">
     <br> 
   <?php }
     }  ?>
